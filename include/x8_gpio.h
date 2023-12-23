6985cc33ac75f1dd26eac9cd35dbb87ac0d2cf14
Serhii Kuzmychov <ku3cmich@gmail.com>
2023-12-23 15:55:46 +0200

    added input

--- include/x8_gpio.h
+++ include/x8_gpio.h
@@ -4,6 +4,8 @@
 #include "esp_err.h"
 #include "driver/gpio.h"
 
+esp_err_t set_gpio_pin_in_low(gpio_num_t num, gpio_int_type_t int_type);
 esp_err_t set_gpio_pin_output(gpio_num_t num);
+esp_err_t gpio_install_isr();
 
 #endif
--- src/x8_gpio.c
+++ src/x8_gpio.c
@@ -2,12 +2,31 @@
 
 esp_err_t set_gpio_pin_output(gpio_num_t num)
 {
-    gpio_config_t io_conf;
-    io_conf.intr_type = GPIO_INTR_DISABLE;
-    io_conf.mode = GPIO_MODE_OUTPUT;
-    io_conf.pin_bit_mask = 1 << num;
-    io_conf.pull_down_en = 0;
-    io_conf.pull_up_en = 0;
+    gpio_config_t io_conf = {
+        .intr_type = GPIO_INTR_DISABLE,
+        .mode = GPIO_MODE_OUTPUT,
+        .pin_bit_mask = 1 << num,
+        .pull_down_en = 0,
+        .pull_up_en = 0
+    };
+
+    return gpio_config(&io_conf);
+}
+
+esp_err_t gpio_install_isr()
+{
+    return gpio_install_isr_service(0);
+}
+
+esp_err_t set_gpio_pin_in_low(gpio_num_t num, gpio_int_type_t int_type)
+{
+    gpio_config_t io_conf = {
+        .intr_type = int_type,
+        .mode = GPIO_MODE_INPUT,
+        .pin_bit_mask = 1 << num,
+        .pull_down_en = 0,
+        .pull_up_en = 1
+    };
 
     return gpio_config(&io_conf);
 }
